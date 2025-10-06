e53c45fdc0b6464f7e976e94bbd31c315b3e2984
Victor Pirottin <vpirotti@z4r6p1.42lyon.fr>
2025-10-06 13:34:29 +0200

    ready to start

---
+++ Makefile
@@ -0,0 +1,62 @@
+SRC_DIR         = src/
+OBJ_DIR         = objs/
+INC_DIR         = header/
+
+PARS_DIR        = $(SRC_DIR)parsing/
+PARS_OBJDIR     = $(OBJ_DIR)parsing/
+
+LIB             = $(SRC_DIR)/libft/libft.a
+
+PARS_FILES      =	
+
+SRC_FILES       =	main.c	\
+
+SOURCES         = $(addprefix $(SRC_DIR), $(SRC_FILES)) \
+                  $(addprefix $(PARS_DIR), $(PARS_FILES)) \
+
+OBJS            = $(addprefix $(OBJ_DIR), $(SRC_FILES:.c=.o)) \
+                  $(addprefix $(PARS_OBJDIR), $(PARS_FILES:.c=.o)) \
+
+HEADER          = $(INC_DIR)cub3D.h
+NAME            = cub3D
+
+CC              = cc
+FLAGS           = -Wall -Wextra -Werror -I$(INC_DIR)
+
+all: $(OBJ_DIR) $(PARS_OBJDIR) $(NAME)
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(PARS_OBJDIR):
+	mkdir -p $(PARS_OBJDIR)
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.c $(HEADER)
+	mkdir -p $(OBJ_DIR)
+	$(CC) $(FLAGS) -c $< -o $@
+
+$(PARS_OBJDIR)%.o: $(PARS_DIR)%.c $(HEADER)
+	mkdir -p $(PARS_OBJDIR)
+	$(CC) $(FLAGS) -c $< -o $@
+
+
+$(NAME): $(OBJS) $(LIB) $(HEADER) Makefile
+	$(CC) $(FLAGS) -o $@ $(OBJS) $(LIB)
+
+$(LIB): FORCE
+	make -C $(SRC_DIR)libft
+
+FORCE:
+
+clean:
+	make clean -C $(SRC_DIR)libft/
+	rm -rf $(OBJ_DIR)
+
+fclean: clean
+	make fclean -C $(SRC_DIR)libft/
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re FORCE
+
---
+++ header/cub3D.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaysant <lpaysant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 08:31:36 by vpirotti          #+#    #+#             */
+/*   Updated: 2025/08/07 12:46:22 by lpaysant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# define CLEAR "\033[H\033[J"
+# define YELLOW "\033[33m"
+# define RESET "\033[0m"
+# define BLUE "\033[34m"
+# define GREEN "\033[0;32m"
+# define CYAN "\033[36m"
+# define RED "\033[31m"
+# define BOLD "\033[1m"
+# define UNDER "\033[4m"
+# define BLINK "\033[5m"
+# define REVERSE "\033[7m"
+# define MAP_MAX 100
+
+//-------------------------------INCLUDE-------------------------------
+# include "../src/libft/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/param.h>
+# include <sys/resource.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+//---------------------------GLOBAL VARIABLES--------------------------
+
+//-------------------------------STRUCTS-------------------------------
+
+#endif
